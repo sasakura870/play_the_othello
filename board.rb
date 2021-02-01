ae3c131fd6cb9ecb629f93d88de4e5a370bdbd2e
sasakura_870 <a_20100409@yahoo.co.jp>
2021-02-01 13:34:36 +0900

    一旦push

--- board.rb
+++ board.rb
@@ -4,16 +4,31 @@
 class Board
   BOARD_SIZE = 8
   MARGIN_SPACE = 2
-  @grid = nil
+  @grid = []
 
   def initialize
     mass_size = BOARD_SIZE + MARGIN_SPACE
-    @grid = Array.new(mass_size, Array.new(mass_size, '0'))
-    @grid.each do |mass|
-      print mass
+    mass_size.times do |y|
+      @grid.push []
+      mass_size.times do |x|
+        @grid[y].push(Mass.new(x: x, y: y))
+      end
+    end
+    # @grid = Array.new(mass_size, Array.new(mass_size, '0'))
+    @grid.each do |side_line|
+      side_line.each do |mass|
+        print mass.char
+      end
       print "\n"
     end
   end
 
-  def put(x:, y:); end
+  private
+
+  def clear_grid
+    # @grid[3, 3] = Stone.new is_black: false
+    # @grid[4, 3] = Stone.new is_black: true
+    # @grid[4, 4] = Stone.new is_black: false
+    # @grid[3, 4] = Stone.new is_black: true
+  end
 end
